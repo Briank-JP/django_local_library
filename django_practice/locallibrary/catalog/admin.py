991945fd69a5704a1086c662636cdb5949e6c664
Kaheru <kaherubrian2016@gmail.com>
2025-02-14 13:55:52 +0300

    modified the models and adde the to the admin file

--- django_practice/locallibrary/catalog/admin.py
+++ django_practice/locallibrary/catalog/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Genre, Book, Author, Language, BookInstance
 
 # Register your models here.
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'id ', 'due_back', 'status')
+    list_filter = ('status', 'due_back')
+    search_fields = ('title')
+    
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+    
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name', 'date_of_birth', 'date_of_death')
+    search_fields = ('last_name', 'first_name')
+    inlines = [BookInline]
+
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
